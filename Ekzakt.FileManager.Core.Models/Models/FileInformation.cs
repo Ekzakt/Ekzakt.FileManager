e13092f8878577237a8688b09e1e0151cb630898
EricJansen <EricJansen@LAPTOP-EJ>
2024-05-06 14:36:55 +0200

    Obsoleted FileProperties class.

--- Ekzakt.FileManager.Core.Models/Models/FileInformation.cs
+++ Ekzakt.FileManager.Core.Models/Models/FileInformation.cs
@@ -1,7 +1,6 @@
 ﻿namespace Ekzakt.FileManager.Core.Models;
 
-[Obsolete("Use FileInformation instead. This class will be removed in a future version.")]
-public class FileProperties
+public class FileInformation
 {
     public string Name { get; set; } = string.Empty;
 
